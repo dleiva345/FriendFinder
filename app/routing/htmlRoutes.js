00edae9142d79769a8abd52800edbc9dee2d9b01
Debbie Leiva <debbieleiva@yahoo.com>
2018-05-15 23:48:36 -0400

    needs updates

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,16 @@
 
-var path = require("path");
+var path = require('path');
 
 module.exports = function(app) {
-    app.get("/survey", function(req, res) {
-        res.sendFile(path.join(_dirname, "../public/survey.html"));
-    })
-    app.use("/home", function(req, res) {
-        res.sendFile(path.join(_dirname, "../public/home.htm"));
+
+    // GET route for /survey returns survey.html.
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname + '/../public/survey.html'));
     });
+
+    // USE route returns home.html for any undefined GET routes.
+    app.use(function (req, res) {
+        res.sendFile(path.join(__dirname + '/../public/home.html'));
+    });
+
 };
